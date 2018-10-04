83e1dffee9013981de410839cf72413106c403d6
Daigo Uenuma <go26dev@gmail.com>
2018-10-04 23:32:02 +0900

    metatag設定

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.3.7'
 
+gem 'meta-tags'
 gem 'kaminari'
 gem 'carrierwave'
 gem 'mini_magick'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'BrushApp',
+      reverse: true,
+      charset: 'utf-8',
+      title: 'プロダクトをSNSに簡単にシェアでき、フィードバックを管理できるサービス',
+      description: 'プロダクトをSNSに簡単にシェアでき、フィードバックを管理できるサービス',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: 'BrushApp',
+        type: 'website',
+        title: 'プロダクトをSNSに簡単にシェアでき、フィードバックを管理できるサービス | BrushApp',
+        description: 'プロダクトをSNSに簡単にシェアでき、フィードバックを管理できるサービス',
+        url: request.original_url,
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@brushapp_me',
+      }
+    }
+  end
 end
