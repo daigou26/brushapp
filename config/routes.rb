ca46fe5a72ff15fe466a354c5f169b8c8df0d176
Daigo Uenuma <go26dev@gmail.com>
2018-10-09 03:55:55 +0900

    route整理、view修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   post '/users/:nickname/posts/new' => "posts#create"
   get '/users/:nickname/posts/:id/edit' => "posts#edit"
   patch '/users/:nickname/posts/:id/edit' => "posts#update"
-  get '/users/:nickname/posts/:id' => "feedbacks#new"
-  post '/users/:nickname/posts/:id' => "feedbacks#create"
+  get '/users/:nickname/posts/:id/feedbacks' => "feedbacks#new"
+  post '/users/:nickname/posts/:id/feedbacks' => "feedbacks#create"
   root 'top#index'
 
   devise_for :users, controllers: {
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: [] do
-    resources :posts, only: [:new, :create, :edit, :update, :destroy]
-    resources :feedbacks, only: [:new, :create, :show]
+    resources :posts, only: [:show, :destroy]
     resources :notifications, only: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
