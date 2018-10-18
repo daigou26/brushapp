9d0311b581c7d0b37513e897982f61cf86e88f71
Daigo Uenuma <go26dev@gmail.com>
2018-10-18 15:43:31 +0900

    top view 更新

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,15 @@
 class TopController < ApplicationController
   def index
-    @feed_items = Post.where("rating >= ?", 4).order("rating desc").limit(6)
-    @new_feed_items = Post.order("created_at desc").limit(6)
+    # tab切り替え
+    feed_type = request.fullpath.split("/").last
+
+    if feed_type == nil
+      @feed_items = Post.where("rating >= ?", 4).order("rating desc").limit(6)
+      @new_feed_items = Post.order("created_at desc").limit(6)
+    elsif feed_type.include?("type=rating")
+      @feed_items = Post.where("rating >= ?", 4).order("rating desc").limit(60).page(params[:page]).per(12)
+    elsif feed_type.include?("type=new")
+      @feed_items = Post.order("created_at desc").limit(60).page(params[:page]).per(12)
+    end
   end
 end
