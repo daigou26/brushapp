fc6f6463d7b98a514635c0e6cb6d050289c300ca
Daigo Uenuma <go26dev@gmail.com>
2018-10-18 17:52:52 +0900

    bug

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,15 +5,12 @@ class TopController < ApplicationController
     now = Time.current
 
     if feed_type == nil
-      @new_feed_items = Post.limit(6).order("created_at desc")
+      @new_feed_items = Post.where("created_at >= ?", now.ago(14.days)).limit(6).order("created_at desc")
       @feed_items = Post.where("rating >= ?", 4).limit(6).order("rating desc")
     elsif feed_type.include?("type=rating")
       @feed_items = Post.where("rating >= ?", 4).order("rating desc").page(params[:page]).per(12)
     elsif feed_type.include?("type=new")
       @feed_items = Post.where("created_at >= ?", now.ago(14.days)).order("created_at desc").page(params[:page]).per(12)
-    else
-      @feed_items = Post.where("rating >= ?", 4).limit(6).order("rating desc")
-      @new_feed_items = Post.limit(6).order("created_at desc")
     end
   end
 end
