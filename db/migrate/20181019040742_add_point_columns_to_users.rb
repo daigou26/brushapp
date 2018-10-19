7ab7fc641af2cff17f8cd837762b9040ecf7bc6c
Daigo Uenuma <go26dev@gmail.com>
2018-10-19 13:23:42 +0900

    userにpointカラムを追加

---
+++ db/migrate/20181019040742_add_point_columns_to_users.rb
@@ -0,0 +1,5 @@
+class AddPointColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :point, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_022538) do
+ActiveRecord::Schema.define(version: 2018_10_19_040742) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.text "content"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_022538) do
     t.string "name"
     t.string "nickname"
     t.string "image"
+    t.integer "point", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
